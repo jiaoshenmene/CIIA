446ec1d77b2a374ae11bd77c5e6b75fe5c0241a0
dujia <815319775@qq.com>
2017-07-05 10:32:59 +0800

    first commit

---
+++ CIIA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CIIA
+//
+//  Created by dujia on 05/07/2017.
+//  Copyright © 2017 dujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CIIA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CIIA
+//
+//  Created by dujia on 05/07/2017.
+//  Copyright © 2017 dujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
